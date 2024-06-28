6a05f7ce854d1f210aa6b0f6190c5c2882049f49
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2024-06-29 01:04:41 +0530

    Add files via upload

---
+++ src/data/songs.js
@@ -0,0 +1,47 @@
+export default [
+	{
+		id: 1,
+		title: 'Demons - Imagine Dragons',
+		description: 'Original Soundtrack',
+		artist: 'Tayfun Abi',
+		image: 'https://i.scdn.co/image/ab67616d0000b273407bd04707c463bbb3410737',
+		type: 'album',
+		src: 'https://freesound.org/data/previews/612/612095_5674468-lq.mp3'
+	},
+	{
+		id: 2,
+		title: 'Wolves',
+		artist: 'Selena Gomez',
+		description: 'Relax and indulge with beautiful piano pieces',
+		image: 'https://i.scdn.co/image/ab67616d0000b273307910d4242c0d6b1fedf955',
+		type: 'album',
+		src: 'https://freesound.org/data/previews/612/612092_7037-lq.mp3'
+	},
+	{
+		id: 3,
+		title: 'Hang Massive',
+		artist: 'Mert Abi',
+		description: 'Artist',
+		image: 'https://i.scdn.co/image/ab67616100005174802686196d39eb0b7b5cd8b1',
+		type: 'artist',
+		src: 'https://freesound.org/data/previews/612/612087_1648170-lq.mp3'
+	},
+	{
+		id: 4,
+		title: 'Old Town Road',
+		artist: 'Lil Nas X',
+		description: 'Original Soundtrack',
+		image: 'https://media.gq.com/photos/5cdeef0e1f8a4e271cddab8d/master/pass/Old-Town-Road-Video-GQ-2019-051719.jpg',
+		type: 'podcast',
+		src: 'https://freesound.org/data/previews/612/612085_28867-lq.mp3'
+	},
+	{
+		id: 5,
+		title: 'Taylor Swift',
+		artist: 'Mehmet Abi',
+		description: 'Original Soundtrack',
+		image: 'https://i1.sndcdn.com/artworks-lPlyPwIwiH76-0-t500x500.jpg',
+		type: 'album',
+		src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'
+	}
+]
